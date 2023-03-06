d4fc5297123c4f6cf43789be25b12059de54dcf7
alifeee <alifeeerenn@hotmail.com>
2023-03-06 11:17:25 +0000

    Return 64bit url from api

--- api/api.py
+++ api/api.py
@@ -1,17 +1,25 @@
 # Web api using Flask, to return simple data
 from flask import Flask
 from flask_restful import Resource, Api
+import re
+import requests
+from steam_api import isVanityUrl, get64BitFromVanityUrl
 
 app = Flask(__name__)
 api = Api(app)
 
 
 class GetImage(Resource):
-    def get(self, profile):
-        return {"profile": profile}
+    def get(self, profile_string):
+        if isVanityUrl(profile_string):
+            profile_id = get64BitFromVanityUrl(profile_string)
+        else:
+            profile_id = profile_string
+
+        return {"success": "true", "profile_id": profile_id}
 
 
-api.add_resource(GetImage, '/steamcollage/<string:profile>')
+api.add_resource(GetImage, '/steamcollage/<string:profile_string>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
