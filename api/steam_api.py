84c9e65ca643f97801fb7eb0a254bf5e3e7e3992
alifeee <alifeeerenn@hotmail.com>
2023-03-06 11:17:13 +0000

    add vanity url conversion

--- api/steam_api.py
+++ api/steam_api.py
@@ -1,4 +1,5 @@
 import re
+import requests
 
 with open("api_key.txt", "r") as f:
     API_KEY = f.read()
@@ -8,3 +9,17 @@ def isVanityUrl(profile_string):
     return re.match(r'^[0-9]{17}$', profile_string) is None
 
 
+def get64BitFromVanityUrl(vanity_url):
+    url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
+    params = {
+        "key": API_KEY,
+        "vanityurl": vanity_url
+    }
+    r = requests.get(url, params=params)
+    json = r.json()
+    if "response" not in json:
+        raise Exception(f"No response in json: {json}")
+    response = json["response"]
+    if "steamid" not in response:
+        raise Exception(f"No steamid in response: {response}")
+    return response["steamid"]
--- api/steam_api_test.py
+++ api/steam_api_test.py
@@ -1,5 +1,11 @@
 import steam_api
 import unittest
+import responses
+from responses import matchers
+
+with open("api_key.txt", "r") as f:
+    API_KEY = f.read()
+
 
 class TestIsVanityUrl(unittest.TestCase):
     def test_vanityUrl(self):
@@ -11,3 +17,55 @@ class TestIsVanityUrl(unittest.TestCase):
     def test_64biturl(self):
         self.assertFalse(steam_api.isVanityUrl("76561198099426919"))
 
+
+baseurl = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
+baseparams = {
+    "key": API_KEY,
+}
+
+
+class TestGet64BitFromVanityUrl(unittest.TestCase):
+    @responses.activate
+    def test_get64BitFromVanityUrl(self):
+        params = baseparams.copy()
+        params["vanityurl"] = "alifeee"
+        responses.get(
+            baseurl,
+            match=[matchers.query_param_matcher(params)],
+            json={
+                "response": {
+                    "steamid": "76561008099426919",
+                    "success": 1
+                }
+            })
+        self.assertEqual(steam_api.get64BitFromVanityUrl(
+            "alifeee"), "76561008099426919")
+
+    @responses.activate
+    def test_nonExistentUrl(self):
+        params = baseparams.copy()
+        params["vanityurl"] = "alifeee"
+        responses.get(
+            baseurl,
+            match=[matchers.query_param_matcher(params)],
+            json={
+                "response": {
+                    "success": 42,
+                    "message": "No match"
+                }
+            })
+        self.assertRaises(
+            Exception, steam_api.get64BitFromVanityUrl, "alifeee")
+
+    @responses.activate
+    def test_emptyQuery(self):
+        params = baseparams.copy()
+        params["vanityurl"] = ""
+        # returns <html><head><title>Bad Request</title></head><body><h1>Bad Request</h1>Required
+        # parameter 'vanityurl' is missing</body></html>
+        responses.get(
+            baseurl,
+            match=[matchers.query_param_matcher(params)],
+            status=400)
+        self.assertRaises(
+            Exception, steam_api.get64BitFromVanityUrl, "")
