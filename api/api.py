f2ec6bd1d8c246db9aafddcae6c68d6fe015727d
alifeee <alifeeerenn@hotmail.com>
2023-03-06 11:32:35 +0000

    Remove flask_restful

--- api/api.py
+++ api/api.py
@@ -1,25 +1,21 @@
 # Web api using Flask, to return simple data
 from flask import Flask
-from flask_restful import Resource, Api
-import re
-import requests
 from steam_api import isVanityUrl, get64BitFromVanityUrl
+from markupsafe import escape
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class GetImage(Resource):
-    def get(self, profile_string):
-        if isVanityUrl(profile_string):
-            profile_id = get64BitFromVanityUrl(profile_string)
-        else:
-            profile_id = profile_string
+@app.route('/steamcollage/<string:profile_string>')
+def get(profile_string):
+    profile_string = escape(profile_string)
+    if isVanityUrl(profile_string):
+        profile_id = get64BitFromVanityUrl(profile_string)
+    else:
+        profile_id = profile_string
 
-        return {"success": "true", "profile_id": profile_id}
+    return {"success": "true", "profile_id": profile_id}
 
-
-api.add_resource(GetImage, '/steamcollage/<string:profile_string>')
 
 if __name__ == '__main__':
     app.run(debug=True)
