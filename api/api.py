fccc5bfaa142172278d20ff5ea06e35e00894771
alifeee <alifeeerenn@hotmail.com>
2023-03-06 13:40:38 +0000

    Change API to use query parameters

--- api/api.py
+++ api/api.py
@@ -1,14 +1,14 @@
 # Web api using Flask, to return simple data
-from flask import Flask
+from flask import Flask, request
 from steam_api import isVanityUrl, get64BitFromVanityUrl
 from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route('/steamcollage/<string:profile_string>')
-def get(profile_string):
-    profile_string = escape(profile_string)
+@app.route('/steamcollage')
+def get():
+    profile_string = request.args.get('profile_string')
     if isVanityUrl(profile_string):
         profile_id = get64BitFromVanityUrl(profile_string)
     else:
