45609225baef152d07717eccc77e8f8da547fe09
alifeee <alifeeerenn@hotmail.com>
2023-03-06 14:16:37 +0000

    Make Flask API return games

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 # Web api using Flask, to return simple data
 from flask import Flask, request
-from steam_api import isVanityUrl, get64BitFromVanityUrl
+from steam_api import isVanityUrl, get64BitFromVanityUrl, getGamesFromSteamId, getImageUrlForGameId
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -10,11 +10,24 @@ app = Flask(__name__)
 def get():
     profile_string = request.args.get('profile_string')
     if isVanityUrl(profile_string):
-        profile_id = get64BitFromVanityUrl(profile_string)
+        try:
+            profile_id = get64BitFromVanityUrl(profile_string)
+        except Exception as e:
+            return f"Error: {e}", 500
     else:
         profile_id = profile_string
 
-    return {"success": "true", "profile_id": profile_id}
+    try:
+        games, game_count = getGamesFromSteamId(profile_id)
+    except Exception as e:
+        return f"Error: {e}", 500
+    for game in games:
+        game["img_icon_url"] = getImageUrlForGameId(game["appid"])
+
+    return {
+        "games": games,
+        "game_count": game_count
+    }
 
 
 if __name__ == '__main__':
