5d9e367ad4f62da88e6fc0a960866986209eb9ab
alifeee <alifeeerenn@hotmail.com>
2023-03-06 13:48:23 +0000

    add getImageUrlForGameId

--- api/steam_api.py
+++ api/steam_api.py
@@ -89,3 +89,15 @@ def getGamesFromSteamId(steam_id: str):
     if "game_count" not in response:
         raise Exception(f"No game_count in response: {response}")
     return response["games"], response["game_count"]
+
+
+def getImageUrlForGameId(game_id: int):
+    """Get image url for game id
+
+    Args:
+        game_id (int): Game id
+
+    Returns:
+        str: Image url
+    """
+    return f"https://cdn.cloudflare.steamstatic.com/steam/apps/{game_id}/header.jpg"
--- api/steam_api_test.py
+++ api/steam_api_test.py
@@ -140,3 +140,20 @@ class TestGetGamesFromSteamId(unittest.TestCase):
             status=500)
         self.assertRaises(
             Exception, steam_api.getGamesFromSteamId, "invalid")
+
+
+class TestGetImageUrlForGameId(unittest.TestCase):
+    def setUp(self):
+        self.baseurl = "https://cdn.cloudflare.steamstatic.com/steam/apps/{game_id}/header.jpg"
+
+    def test_getImageUrlForGameId(self):
+        game_id = 10
+        self.assertEqual(
+            steam_api.getImageUrlForGameId(game_id),
+            self.baseurl.format(game_id=game_id))
+
+    def test_getImageUrlForGameId2(self):
+        game_id = 205560
+        self.assertEqual(
+            steam_api.getImageUrlForGameId(game_id),
+            self.baseurl.format(game_id=game_id))
