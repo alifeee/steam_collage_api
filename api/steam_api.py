2b3899314f951418bf3f7bd2cc26b56166c447b5
alifeee <alifeeerenn@hotmail.com>
2023-03-06 13:41:43 +0000

    Improve docstring and typing

--- api/steam_api.py
+++ api/steam_api.py
@@ -5,11 +5,30 @@ with open("api_key.txt", "r") as f:
     API_KEY = f.read()
 
 
-def isVanityUrl(profile_string):
+def isVanityUrl(profile_string: str):
+    """Check if profile string is a vanity url
+
+    Args:
+        profile_string (str): Profile string, e.g. "alifeee" or "5164879451874"
+
+    Returns:
+        bool: True if profile string is a vanity url
+    """
     return re.match(r'^[0-9]{17}$', profile_string) is None
 
 
-def get64BitFromVanityUrl(vanity_url):
+def get64BitFromVanityUrl(vanity_url: str):
+    """Get 64 bit steam id from vanity url
+
+    Args:
+        vanity_url (str): Vanity url extension, e.g. "alifeee"
+
+    Raises:
+        Exception: If API response is invalid
+
+    Returns:
+        str: 64 bit steam id
+    """
     url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
     params = {
         "key": API_KEY,
