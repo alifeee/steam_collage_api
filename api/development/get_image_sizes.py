6cad718a7cd9d4429a9b86d98818682878554dea
alifeee <alifeeerenn@hotmail.com>
2023-03-06 15:54:58 +0000

    add image commands

---
+++ api/development/get_image_sizes.py
@@ -0,0 +1,28 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from steam_api import getGamesFromSteamId, get64BitFromVanityUrl
+from images import getImageForGameId
+from tqdm import tqdm
+
+
+with open("api_key.txt", "r") as f:
+    API_KEY = f.read()
+
+profile_id = get64BitFromVanityUrl(API_KEY, "alifeee")
+games, tot = getGamesFromSteamId(API_KEY, profile_id)
+
+sizes = {}
+for game in tqdm(games, desc="Getting image sizes", total=len(games), unit="images"):
+    image = getImageForGameId(game["appid"])
+    if image.size in sizes:
+        sizes[image.size].append(game["appid"])
+    else:
+        sizes[image.size] = [game["appid"]]
+
+print("Image sizes:")
+for size in sizes:
+    if len(sizes[size]) > 1:
+        print(f"{size}: {len(sizes[size])} games")
+    else:
+        print(f"{size}: 1 game: {sizes[size][0]}")
--- api/images.py
+++ api/images.py
@@ -1,3 +1,17 @@
+# Grabs image from url, or from cache if it exists
+
+import os
+import requests
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from tqdm import tqdm
+from io import BytesIO
+from flask import send_file
+
+CACHE_DIR = "./cache/"
+if not os.path.exists(CACHE_DIR):
+    os.mkdir(CACHE_DIR)
+
 
 def getImageUrlForGameId(game_id: int):
     """Get image url for game id
@@ -10,3 +24,63 @@ def getImageUrlForGameId(game_id: int):
     """
     return f"https://cdn.cloudflare.steamstatic.com/steam/apps/{game_id}/header.jpg"
 
+
+def getImageForGameId(game_id: int):
+    """Get image for game id
+
+    Args:
+        game_id (int): Game id
+
+    Returns:
+        Image: Image (PIL)
+    """
+    url = getImageUrlForGameId(game_id)
+    img_path = CACHE_DIR + str(game_id) + ".jpg"
+    if os.path.exists(img_path):
+        return Image.open(img_path)
+    else:
+        response = requests.get(url)
+        try:
+            img = Image.open(BytesIO(response.content))
+        except:
+            print("Error opening image for game id: " + str(game_id))
+            img = Image.new("RGB", (460, 215))
+            img_draw = ImageDraw.Draw(img)
+            img_draw.text((0, 0), "Error", fill=(255, 255, 255))
+        img.save(img_path)
+        return img
+
+
+def makeCollage(games, aspect_ratio=1, max_images=-1):
+    if max_images == -1:
+        max_images = len(games)
+    images = []
+    for game in tqdm(games[:max_images], desc="Downloading images", total=max_images, unit="images"):
+        images.append(getImageForGameId(game["appid"]))
+    THUMB_WIDTH = 460
+    THUMB_HEIGHT = 215
+    # find how many rows and columns we need
+    num_images = len(images)
+    num_cols = int(num_images ** 0.5)
+    num_rows = int(num_images / num_cols)
+    if num_rows * num_cols < num_images:
+        num_rows += 1
+    # create a new image with the right size
+    collage_width = num_cols * THUMB_WIDTH
+    collage_height = num_rows * THUMB_HEIGHT
+    collage = Image.new("RGB", (collage_width, collage_height))
+    # paste the images into the right position
+    for i, image in enumerate(images):
+        row = i // num_cols
+        col = i % num_cols
+        x = col * THUMB_WIDTH
+        y = row * THUMB_HEIGHT
+        collage.paste(image, (x, y))
+    return collage
+
+
+def serve_pil_image(pil_img: Image):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
