41b7f3a1c110a6eb45b6353824088274e9a67a2a
alifeee <alifeeerenn@hotmail.com>
2023-03-12 15:11:56 +0000

    Add tests for images

--- api/images_test.py
+++ api/images_test.py
@@ -1,5 +1,8 @@
 import images
 import unittest
+import responses
+from PIL import Image
+from parameterized import parameterized
 
 
 class TestGetImageUrlForGameId(unittest.TestCase):
@@ -17,3 +20,37 @@ class TestGetImageUrlForGameId(unittest.TestCase):
         self.assertEqual(
             images.getImageUrlForGameId(game_id),
             self.baseurl.format(game_id=game_id))
+
+
+test_image_folder = "api/test_images/"
+game_ids = [
+    "440",
+    "207140",
+    "213650",
+    "226100",
+    "247000",
+]
+game_images = [
+    Image.open(test_image_folder + game_id + ".jpg")
+    for game_id in game_ids
+]
+
+
+class TestBytesFromPilImage(unittest.TestCase):
+    def test_bytesFromPilImage(self):
+        self.skipTest("Not implemented yet")
+
+
+class TestGetImageForGameId(unittest.TestCase):
+    @parameterized.expand(zip(game_ids, game_images))
+    def test_getImageForGameId(self, game_id, expected_image):
+        for image, id in zip(game_images, game_ids):
+            responses.add(
+                responses.GET,
+                images.getImageUrlForGameId(id),
+                body=image.tobytes(),
+                status=200,
+                content_type="image/jpeg",
+            )
+        img = images.getImageForGameId(game_id)
+        self.assertEqual(img, expected_image)
